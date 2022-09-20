478b2cd454bdd529d12d68467a7bc3e5267a657c
robykartis <robykarti.s@gmail.com>
2022-09-21 01:20:55 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+
         $this->call(UserSeeder::class);
     }
 }
